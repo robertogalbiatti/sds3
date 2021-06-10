b80a1234ac63f4954d40e0de0e929bf06263d219
Roberto <roberto.galbiattifilho@br.bosch.com>
2021-06-10 13:10:36 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import { SalePage } from "types/sale";
 import { BASE_URL } from "utils/requests";
 import { formatLocalDate } from "utils/format";
+import Pagination from "components/Pagination";
 
 const DataTable = () => { /*Função anônima/Lambda, estamos atribuindo a ela o NavBar */
 
+  const [activePage, setActivePage] = useState(0);
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -15,37 +17,44 @@ const DataTable = () => { /*Função anônima/Lambda, estamos atribuindo a ela o
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+    axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
       .then(response => {
-          setPage(response.data);
+        setPage(response.data);
       });
-  }, []);
+  }, [activePage]);
+  //A página ativa passa a ser esse index!
+  const changePage = (index: number) => {
+    setActivePage(index);
+  }
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes Visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {page.content?.map(item => ( //particularidade do React utilizar o key quando está renderizando uma coleção (vide one note)
-            <tr key={item.id}> 
-            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
-            <td>{item.seller.name}</td>
-            <td>{item.visited}</td>
-            <td>{item.deals}</td>
-            <td>{item.amount.toFixed(2)}</td>
-          </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Pagination page={page} onPageChange={changePage}/>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes Visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {page.content?.map(item => ( //particularidade do React utilizar o key quando está renderizando uma coleção (vide one note)
+              <tr key={item.id}>
+                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                <td>{item.seller.name}</td>
+                <td>{item.visited}</td>
+                <td>{item.deals}</td>
+                <td>{item.amount.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
